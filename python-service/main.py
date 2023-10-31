5adb8c8b5729e8cb8bb691b708a9b8ee81e72fc4
srinathperera <hemapani@gmail.com>
2023-10-31 15:26:17 +0530

    Updates

--- python-service/main.py
+++ python-service/main.py
@@ -3,8 +3,8 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/greet', methods=['GET'])
-def greet():
-    return jsonify(message="Hello, World!")
+def greet(name):
+    return jsonify(message="Hello, World! " + name)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
