6bf0b82b505407779fa09ccde2029bfdb5f817c2
srinathperera <hemapani@gmail.com>
2023-11-01 10:16:29 +0530

    updates

--- python-service/main.py
+++ python-service/main.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def greet(name):
     return jsonify(message="Hello, World! " + name)
 
+@app.route('/hello', methods=['POST'])
+def hello(name):
+    user = {
+        "username": name,
+        "email": "john@example.com",
+        "roles": ["admin", "user"],
+        "isActive": True
+    }
+    return jsonify(user)
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
